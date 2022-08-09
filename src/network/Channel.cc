032b331a46127f5401544c059bca2ba4c7f6fcb9
hash-roar <1022647354@qq.com>
2022-08-09 10:32:54 +0800

    finish Channel

--- src/network/Channel.cc
+++ src/network/Channel.cc
@@ -1,9 +1,102 @@
 #include "network/Channel.h"
 
+#include <sys/epoll.h>
+
+#include <cassert>
+#include <sstream>
+
+#include "log/Logger.h"
 #include "network/EventLoop.h"
 
 namespace rnet::Network {
+const int Channel::kNoneEvent = 0;
+const int Channel::kReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::kWriteEvent = EPOLLOUT;
 Channel::Channel(EventLoop* loop, int fd) : loop_(loop), fd_(fd) {}
 
+Channel::~Channel() {
+  assert(!eventHandling_);
+  assert(!addedToLoop_);
+  if (loop_->isInLoopThread()) {
+    assert(!loop_->hasChannel(this));
+  }
+}
+
+void Channel::tie(const std::shared_ptr<void>& obj) {
+  tie_ = obj;
+  tied_ = true;
+}
+
+void Channel::update() {
+  addedToLoop_ = true;
+  loop_->updateChannel(this);
+}
+
+void Channel::remove() {
+  assert(isNoneEvent());
+  addedToLoop_ = false;
+  loop_->removeChannel(this);
+}
+
+void Channel::handleEvent(Unix::Timestamp receiveTime) {
+  std::shared_ptr<void> guard;
+  if (tied_) {
+    guard = tie_.lock();
+    if (guard) {
+      handleEventWithGuard(receiveTime);
+    }
+  } else {  //?
+    handleEventWithGuard(receiveTime);
+  }
+}
+
+void Channel::handleEventWithGuard(Unix::Timestamp receiveTime) {
+  eventHandling_ = true;
+  LOG_TRACE << reventsToString();
+  if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN)) {
+    if (logHup_) {
+      LOG_WARN << "fd = " << fd_ << " Channel::handle_event() POLLHUP";
+    }
+    if (closeCallback_) closeCallback_();
+  }
+
+  //   if (revents_ & POLLNVAL) {
+  //     LOG_WARN << "fd = " << fd_ << " Channel::handle_event() POLLNVAL";
+  //   }
+
+  if (revents_ & (EPOLLERR)) {
+    if (errorCallback_) errorCallback_();
+  }
+  if (revents_ & (EPOLLIN | EPOLLPRI | EPOLLRDHUP)) {
+    if (readCallback_) readCallback_(receiveTime);
+  }
+  if (revents_ & EPOLLOUT) {
+    if (writeCallback_) writeCallback_();
+  }
+  eventHandling_ = false;
+}
+
+std::string Channel::reventsToString() const {
+  return eventsToString(fd_, revents_);
+}
+
+std::string Channel::eventsToString() const {
+  return eventsToString(fd_, events_);
+}
+
+std::string Channel::eventsToString(int fd, int ev) {
+  std::ostringstream oss;
+  oss << fd << ": ";
+  if (ev & EPOLLIN) oss << "IN ";
+  if (ev & EPOLLPRI) oss << "PRI ";
+  if (ev & EPOLLOUT) oss << "OUT ";
+  if (ev & EPOLLHUP) oss << "HUP ";
+  if (ev & EPOLLRDHUP) oss << "RDHUP ";
+  if (ev & EPOLLERR) oss << "ERR ";
+  //   if (ev & POLLNVAL)
+  //     oss << "NVAL ";
+
+  return oss.str();
+}
 
 }  // namespace rnet::Network
--- src/network/EventLoop.h
+++ src/network/EventLoop.h
@@ -1,124 +1,135 @@
 #pragma once
+#include <any>
 #include <functional>
+#include <mutex>
+
 #include "base/Common.h"
+#include "network/Channel.h"
+#include "network/Timer.h"
+#include "network/TimerId.h"
+#include "unix/Thread.h"
 #include "unix/Time.h"
 namespace rnet::Network {
-  class EventLoop : noncopyable {
-   public:
-    // typedef std::function<void()> Functor;
-    using Functor = std::function<void()>;
 
-    EventLoop();
-    ~EventLoop();  // force out-line dtor, for std::unique_ptr members.
+class Epoll;
+class Channel;
+class TimerQueue;
+class EventLoop : noncopyable {
+ public:
+  // typedef std::function<void()> Functor;
+  using Functor = std::function<void()>;
 
-    ///
-    /// Loops forever.
-    ///
-    /// Must be called in the same thread as creation of the object.
-    ///
-    void loop();
+  EventLoop();
+  ~EventLoop();  // force out-line dtor, for std::unique_ptr members.
 
-    /// Quits loop.
-    ///
-    /// This is not 100% thread safe, if you call through a raw pointer,
-    /// better to call through shared_ptr<EventLoop> for 100% safety.
-    void quit();
+  ///
+  /// Loops forever.
+  ///
+  /// Must be called in the same thread as creation of the object.
+  ///
+  void loop();
 
-    ///
-    /// Time when poll returns, usually means data arrival.
-    ///
-    Unix::Timestamp pollReturnTime() const { return pollReturnTime_; }
+  /// Quits loop.
+  ///
+  /// This is not 100% thread safe, if you call through a raw pointer,
+  /// better to call through shared_ptr<EventLoop> for 100% safety.
+  void quit();
 
-    int64_t iteration() const { return iteration_; }
+  ///
+  /// Time when poll returns, usually means data arrival.
+  ///
+  Unix::Timestamp pollReturnTime() const { return pollReturnTime_; }
 
-    /// Runs callback immediately in the loop thread.
-    /// It wakes up the loop, and run the cb.
-    /// If in the same loop thread, cb is run within the function.
-    /// Safe to call from other threads.
-    void runInLoop(Functor cb);
-    /// Queues callback in the loop thread.
-    /// Runs after finish pooling.
-    /// Safe to call from other threads.
-    void queueInLoop(Functor cb);
+  int64_t iteration() const { return iteration_; }
 
-    size_t queueSize() const;
+  /// Runs callback immediately in the loop thread.
+  /// It wakes up the loop, and run the cb.
+  /// If in the same loop thread, cb is run within the function.
+  /// Safe to call from other threads.
+  void runInLoop(Functor cb);
+  /// Queues callback in the loop thread.
+  /// Runs after finish pooling.
+  /// Safe to call from other threads.
+  void queueInLoop(Functor cb);
 
-    // timers
+  size_t queueSize() const;
 
-    ///
-    /// Runs callback at 'time'.
-    /// Safe to call from other threads.
-    ///
-    TimerId runAt(Timestamp time, TimerCallback cb);
-    ///
-    /// Runs callback after @c delay seconds.
-    /// Safe to call from other threads.
-    ///
-    TimerId runAfter(double delay, TimerCallback cb);
-    ///
-    /// Runs callback every @c interval seconds.
-    /// Safe to call from other threads.
-    ///
-    TimerId runEvery(double interval, TimerCallback cb);
-    ///
-    /// Cancels the timer.
-    /// Safe to call from other threads.
-    ///
-    void cancel(TimerId timerId);
+  // timers
 
-    // internal usage
-    void wakeup();
-    void updateChannel(Channel* channel);
-    void removeChannel(Channel* channel);
-    bool hasChannel(Channel* channel);
+  ///
+  /// Runs callback at 'time'.
+  /// Safe to call from other threads.
+  ///
+  TimerId runAt(Unix::Timestamp time, TimerCallback cb);
+  ///
+  /// Runs callback after @c delay seconds.
+  /// Safe to call from other threads.
+  ///
+  TimerId runAfter(double delay, TimerCallback cb);
+  ///
+  /// Runs callback every @c interval seconds.
+  /// Safe to call from other threads.
+  ///
+  TimerId runEvery(double interval, TimerCallback cb);
+  ///
+  /// Cancels the timer.
+  /// Safe to call from other threads.
+  ///
+  void cancel(TimerId timerId);
 
-    // pid_t threadId() const { return threadId_; }
-    void assertInLoopThread() {
-      if (!isInLoopThread()) {
-        abortNotInLoopThread();
-      }
+  // internal usage
+  void wakeup();
+  void updateChannel(Channel* channel);
+  void removeChannel(Channel* channel);
+  bool hasChannel(Channel* channel);
+
+  // pid_t threadId() const { return threadId_; }
+  void assertInLoopThread() {
+    if (!isInLoopThread()) {
+      abortNotInLoopThread();
     }
-    bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
-    // bool callingPendingFunctors() const { return callingPendingFunctors_; }
-    bool eventHandling() const { return eventHandling_; }
+  }
+  bool isInLoopThread() const { return threadId_ == Thread::tid(); }
+  // bool callingPendingFunctors() const { return callingPendingFunctors_; }
+  bool eventHandling() const { return eventHandling_; }
 
-    void setContext(const boost::any& context) { context_ = context; }
+  void setContext(const std::any& context) { context_ = context; }
 
-    const boost::any& getContext() const { return context_; }
+  const std::any& getContext() const { return context_; }
 
-    boost::any* getMutableContext() { return &context_; }
+  std::any* getMutableContext() { return &context_; }
 
-    static EventLoop* getEventLoopOfCurrentThread();
+  static EventLoop* getEventLoopOfCurrentThread();
 
-   private:
-    void abortNotInLoopThread();
-    void handleRead();  // waked up
-    void doPendingFunctors();
+ private:
+  void abortNotInLoopThread();
+  void handleRead();  // waked up
+  void doPendingFunctors();
 
-    void printActiveChannels() const;  // DEBUG
+  void printActiveChannels() const;  // DEBUG
 
-    typedef std::vector<Channel*> ChannelList;
+  using ChannelList = std::vector<Channel*>;
 
-    bool looping_; /* atomic */
-    std::atomic<bool> quit_;
-    bool eventHandling_;          /* atomic */
-    bool callingPendingFunctors_; /* atomic */
-    int64_t iteration_;
-    const pid_t threadId_;
-    Timestamp pollReturnTime_;
-    std::unique_ptr<Poller> poller_;
-    std::unique_ptr<TimerQueue> timerQueue_;
-    int wakeupFd_;
-    // unlike in TimerQueue, which is an internal class,
-    // we don't expose Channel to client.
-    std::unique_ptr<Channel> wakeupChannel_;
-    boost::any context_;
+  bool looping_; /* atomic */
+  std::atomic<bool> quit_;
+  bool eventHandling_;          /* atomic */
+  bool callingPendingFunctors_; /* atomic */
+  int64_t iteration_;
+  const pid_t threadId_;
+  Unix::Timestamp pollReturnTime_;
+  std::unique_ptr<Epoll> poller_;
+  std::unique_ptr<TimerQueue> timerQueue_;
+  int wakeupFd_;
+  // unlike in TimerQueue, which is an internal class,
+  // we don't expose Channel to client.
+  std::unique_ptr<Channel> wakeupChannel_;
+  std::any context_;
 
-    // scratch variables
-    ChannelList activeChannels_;
-    Channel* currentActiveChannel_;
+  // scratch variables
+  ChannelList activeChannels_;
+  Channel* currentActiveChannel_;
 
-    mutable MutexLock mutex_;
-    std::vector<Functor> pendingFunctors_ GUARDED_BY(mutex_);
-  };
-}  // namespace rnet::network
+  mutable std::mutex mutex_;
+  std::vector<Functor> pendingFunctors_;
+};
+}  // namespace rnet::Network
